111a6db67c8622a5838172022a1279db784315b9
Steffen <snevessilva1@my.bcit.ca>
2021-09-10 15:24:29 -0700

    final changes?

--- src/pages/PageFavourite.js
+++ src/pages/PageFavourite.js
@@ -4,26 +4,34 @@ import { useEffect } from 'react';
 import { appTitle } from '../utilities/global';
 import useGlobal from '../store/globalApp';
 import MoviePage from '../components/MoviePage';
+import {useState} from 'react';
+import isFav from '../utilities/isFav';
 
 function PageFavourite() {
 
+    const [ready, setReady]= useState();
+
 	const globalStateAndActions = useGlobal();
     const globalState = globalStateAndActions[0];
+    const globalActions = useGlobal();
     
     useEffect(()=>{
         document.title = `${appTitle} - My Favourites`;
     }, []);
+    useEffect(()=>{
+        setReady(true);
+    },[globalActions]);
 
     return (
         <div className='fav-wrapper'>
             <h1>My Favourites</h1>
-            { globalState.favs.length < 1 ? 
+            { globalState.favs.length === 0 ? 
                 <p style={{height:'34vh', marginTop: '15rem'}}>No favourites, go add some favourites!</p> : 
                 <div className='fav-grid'>
                     {globalState.favs.map((singleMovie, i) => {
                         return <MoviePage key = {i} 
                             movieObj = {singleMovie} 
-                            isFav={true} />
+                            isFav={isFav(globalState.favs, null, singleMovie.id)} />
                     })}
                 </div>
             }
